09041367c4da0e769a42d943307748c9a18ac324
kuujhin <kdoubleu0711@gmail.com>
2024-02-20 16:56:00 +0900

    now playing movies

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -34,22 +34,62 @@ class HomeScreen extends StatelessWidget {
                     return Column(
                       children: [
                         const SizedBox(
-                          height: 50,
+                          height: 15,
                         ),
                         Expanded(
-                          child: makeList(snapshot),
+                          child: makePopularList(snapshot),
                         ),
                       ],
                     );
                   }
-                  return const Center(
-                    child: CircularProgressIndicator(),
+                  return const SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
                   );
                 },
               ),
             ),
+            const SizedBox(
+              height: 15,
+            ),
             const Text(
               'Now in Cinemas',
+              style: TextStyle(
+                fontSize: 26,
+              ),
+            ),
+            Expanded(
+              child: FutureBuilder(
+                future: popularMovies,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Expanded(
+                          child: makeNowPlayingList(snapshot),
+                        ),
+                      ],
+                    );
+                  }
+                  return const SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              'Coming Soon',
               style: TextStyle(
                 fontSize: 26,
               ),
@@ -60,7 +100,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  ListView makeList(AsyncSnapshot<List<MovieSimpleModel>> snapshot) {
+  ListView makePopularList(AsyncSnapshot<List<MovieSimpleModel>> snapshot) {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemCount: snapshot.data!.length,
@@ -78,7 +118,8 @@ class HomeScreen extends StatelessWidget {
               Hero(
                 tag: movie.id,
                 child: Container(
-                  width: 500,
+                  width: 400,
+                  height: 250,
                   clipBehavior: Clip.hardEdge,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
@@ -91,6 +132,9 @@ class HomeScreen extends StatelessWidget {
                       ]),
                   child: Image.network(
                     'https://image.tmdb.org/t/p/w780/${movie.backdrop_path}',
+                    width: 400,
+                    height: 250,
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
@@ -109,6 +153,67 @@ class HomeScreen extends StatelessWidget {
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
+      ),
+    );
+  }
+
+  ListView makeNowPlayingList(AsyncSnapshot<List<MovieSimpleModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 10,
+      ),
+      itemBuilder: (context, index) {
+        // print(index);
+        var movie = snapshot.data![index];
+        return GestureDetector(
+          onTap: () {},
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: movie.id,
+                child: Container(
+                  width: 150,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                      ]),
+                  child: Image.network(
+                    'https://image.tmdb.org/t/p/w780/${movie.backdrop_path}',
+                    width: 150,
+                    height: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 150,
+                child: Text(
+                  movie.title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 20,
       ),
     );
   }
